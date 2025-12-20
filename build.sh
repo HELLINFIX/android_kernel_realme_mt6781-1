a00bec274879cde37b21036a1954320a533ca632
HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>
2025-12-20 14:29:49 +0000

    build_script switch to proton clang

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 clear
 function compile() 
 {
-
 echo COSMOS Kernel, CODENAMED - SPACED
 echo
 sleep 3 >/dev/null
@@ -10,11 +9,12 @@ source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G >/dev/null
 export ARCH=arm64
-export KBUILD_BUILD_HOST=Cyanide
-export KBUILD_BUILD_USER="pritish"
-git clone --depth=1 https://github.com/DrtSinX98/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
-git clone --depth=1 https://github.com/DrtSinX98/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
-git clone --depth=1 https://github.com/DrtSinX98/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
+export KBUILD_BUILD_HOST=COSMOS
+export KBUILD_BUILD_USER="INFIX"
+
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/sarthakroy2002/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
+git clone --depth=1 https://github.com/sarthakroy2002/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
 
 read -p "Wanna do dirty build? (Y/N): " build_type
 if [[ $build_type == "N" || $build_type == "n" ]]; then
@@ -37,24 +37,29 @@ fi
 
 make O=out ARCH=arm64 spaced_defconfig
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC="clang" \
+                      LD=ld.lld \
+		      AR=llvm-ar \
+		      NM=llvm-nm \
+		      OBJCOPY=llvm-objcopy \
+		      OBJDUMP=llvm-objdump \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
+                      CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
                       CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
 }
 
 function zupload()
 {
-git clone --depth=1 https://github.com/DrtSinX98/AnyKernel3.git AnyKernel
+git clone --depth=1 https://github.com/HELLINFIX/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 Cyanide-kernel-2.0-spaced.zip *
+zip -r9 Cosmos-kernel-1.0-spaced.zip *
 curl -sL https://git.io/file-transfer | sh
-./transfer wet Cyanide-kernel-2.0-spaced.zip
+./transfer anon Cosmos-kernel-1.0-spaced.zip
 }
 
 compile
