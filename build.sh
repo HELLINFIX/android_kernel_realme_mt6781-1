90c3d3a113503fcb428a6b97f95cdc047da2baa0
HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>
2025-12-21 18:37:14 +0000

    scripts: Add build script

---
+++ build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+clear
+function compile() 
+{
+echo COSMOS Kernel, CODENAMED - SPACED
+echo
+sleep 3 >/dev/null
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+ccache -M 100G >/dev/null
+export ARCH=arm64
+export KBUILD_BUILD_HOST=COSMOS
+export KBUILD_BUILD_USER="INFIX"
+
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/HELLINFIX/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
+git clone --depth=1 https://github.com/HELLINFIX/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
+
+read -p "Wanna do dirty build? (Y/N): " build_type
+if [[ $build_type == "N" || $build_type == "n" ]]; then
+echo Deleting out directory and doing clean Build
+sleep 3 >/dev/null
+rm -rf out && mkdir -p out
+fi
+if [[ $build_type == "Y" || $build_type == "y" ]]; then
+echo Warning :- Doing dirty build
+sleep 3 >/dev/null
+fi
+if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
+if ! [[ $build_type == "N" || $build_type == "n" ]]; then
+echo Invalid Input , Read carefully before typing
+echo Trying to restart script
+sleep 3 >/dev/null
+. build.sh && exit
+fi
+fi
+
+make O=out ARCH=arm64 spaced_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC="clang" \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
+                      LLVM=1 \
+                      LLVM_IAS=1 \
+                      CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
+}
+
+function zupload()
+{
+git clone --depth=1 https://github.com/HELLINFIX/AnyKernel3.git AnyKernel
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+cd AnyKernel
+zip -r9 Cosmos-kernel-1.0-spaced.zip *
+curl -F "file=@AnyKernel/Cosmos-kernel-1.0-spaced.zip" https://temp.sh/upload
+}
+
+compile
+zupload
